7113ea9772f0e300f62e8d8a31c588db76d12546
«Yuliya <YBelik@renins.com>
2026-01-21 10:24:26 +0300

    Доработан UI, исправлен баг с последовательной загрузкой

--- app/src/main/java/com/ybelik/news/screen/subscriiption/SubscriptionsScreen.kt
+++ app/src/main/java/com/ybelik/news/screen/subscriiption/SubscriptionsScreen.kt
@@ -3,15 +3,17 @@ package com.ybelik.news.screen.subscriiption
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Clear
@@ -25,26 +27,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.Divider
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,6 +56,7 @@ import coil3.compose.AsyncImage
 import com.ybelik.domain.model.Article
 import com.ybelik.news.DateFormatter
 import com.ybelik.news.R
+import com.ybelik.news.ui.theme.CustomIcons
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -62,9 +65,9 @@ fun SubscriptionsScreen(
     viewModel: SubscriptionsViewModel = hiltViewModel(),
     onNavigateToSettings: () -> Unit
 ) {
-    val state = viewModel.state.collectAsState()
+    val state by viewModel.state.collectAsState()
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         topBar = {
             NewsAppBar(
                 onRefreshClick = {
@@ -73,22 +76,23 @@ fun SubscriptionsScreen(
                 onClearClick = {
                     viewModel.handleIntent(SubscriptionsIntent.ClearArticles)
                 },
-                onSettingsClick = {
-                    onNavigateToSettings()
-                }
+                onSettingsClick = onNavigateToSettings
             )
         }
     ) { innerPadding ->
         LazyColumn(
             modifier = modifier
-                .padding(innerPadding)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            contentPadding = innerPadding,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             item {
                 InputTopic(
                     onTextChanged = { topic ->
                         viewModel.handleIntent(SubscriptionsIntent.InputTopic(topic))
                     },
-                    text = state.value.query
+                    text = state.query
                 )
             }
 
@@ -98,20 +102,19 @@ fun SubscriptionsScreen(
                         viewModel.handleIntent(SubscriptionsIntent.ClickSubscribe)
                     },
                     text = stringResource(R.string.add_subscription),
-                    isEnabled = state.value.subscribeBtnEnabled
+                    isEnabled = state.subscribeBtnEnabled
                 )
             }
             item {
                 Text(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    text = stringResource(R.string.subscriptions, state.value.subscriptions.size),
+                    text = stringResource(R.string.subscriptions, state.subscriptions.size),
                     color = MaterialTheme.colorScheme.onSurface,
                     fontSize = 18.sp
                 )
             }
             item {
                 FilterChips(
-                    subscriptions = state.value.subscriptions,
+                    subscriptions = state.subscriptions,
                     onChipClick = { topic ->
                         viewModel.handleIntent(SubscriptionsIntent.ToggleTopicSelection(topic = topic))
                     },
@@ -120,38 +123,51 @@ fun SubscriptionsScreen(
                     }
                 )
             }
-            item {
-                HorizontalDivider(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    thickness = 1.dp,
-                    color = Color.LightGray
-                )
-            }
-            item {
-                Text(
-                    modifier = Modifier.padding(16.dp),
-                    text = stringResource(R.string.articles, state.value.articles.size),
-                    color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = 20.sp
-                )
-            }
-            item {
-                HorizontalDivider(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    thickness = 1.dp,
-                    color = Color.LightGray
-                )
-            }
-            state.value.articles.forEach { article ->
+            if (state.articles.isNotEmpty()) {
                 item {
-                    Article(
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = Color.LightGray
+                    )
+                }
+                item {
+                    Text(
+                        text = stringResource(R.string.articles, state.articles.size),
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontSize = 18.sp
+                    )
+                }
+                item {
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = Color.LightGray
+                    )
+                }
+                items(
+                    items = state.articles,
+                    key = { it.url }
+                ) {
+                    ArticleCard(
                         onReadClick = {
                             // TODO navigate to webview
                         },
                         onShareClick = {
                             // TODO share
                         },
-                        article = article
+                        article = it
+                    )
+                }
+            } else if (state.subscriptions.isNotEmpty()) {
+                item {
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = Color.LightGray
+                    )
+                }
+                item {
+                    Text(
+                        text = stringResource(R.string.no_articles_for_selected_subscription),
+                        textAlign = TextAlign.Center
                     )
                 }
             }
@@ -243,8 +259,7 @@ fun InputTopic(
 ) {
     OutlinedTextField(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
+            .fillMaxWidth(),
         value = text,
         onValueChange = onTextChanged,
         singleLine = true,
@@ -281,7 +296,6 @@ fun ActionButton(
 ) {
     Button(
         modifier = modifier
-            .padding(16.dp)
             .fillMaxWidth(),
         onClick = {
             onButtonClick()
@@ -291,12 +305,10 @@ fun ActionButton(
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = stringResource(R.string.add_subscription_btn),
-            tint = MaterialTheme.colorScheme.onSurface,
         )
         Text(
             modifier = Modifier.padding(horizontal = 8.dp),
             text = text,
-            color = MaterialTheme.colorScheme.onSurface
         )
     }
 }
@@ -322,41 +334,38 @@ fun FilterChips(
     onChipClick: (String) -> Unit,
     onDeleteChipClick: (String) -> Unit
 ) {
-    Row(
-        modifier = modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        subscriptions.entries.forEach { entry ->
-            FilterChip(
-                selected = entry.value,
-                onClick = {
-                    onChipClick(entry.key)
-                },
-                label = {
-                    Text(text = entry.key)
-                },
-                trailingIcon = {
-                    Icon(
-                        modifier = Modifier
-                            .size(FilterChipDefaults.IconSize)
-                            .clickable {
-                                onDeleteChipClick(entry.key)
-                            },
-                        imageVector = Icons.Default.Clear,
-                        contentDescription = stringResource(R.string.clear_chip)
+    if (subscriptions.isNotEmpty()) {
+        LazyRow(
+            modifier = modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            subscriptions.forEach { (topic, isSelected) ->
+                item(key = topic) {
+                    SubscriptionChip(
+                        topic = topic,
+                        isSelected = isSelected,
+                        onChipClick = onChipClick,
+                        onDeleteChipClick = onDeleteChipClick
                     )
                 }
-            )
+            }
         }
+    } else {
+        Text(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            text = "No subscriptions",
+            textAlign = TextAlign.Center
+        )
     }
 }
 
 @Composable
 @Preview
 fun ArticlePreview() {
-    Article(
+    ArticleCard(
         onReadClick = {},
         onShareClick = {},
         article = Article(
@@ -371,30 +380,30 @@ fun ArticlePreview() {
 }
 
 @Composable
-fun Article(
+fun ArticleCard(
     modifier: Modifier = Modifier,
     onReadClick: () -> Unit,
     onShareClick: () -> Unit,
     article: Article
 ) {
-    Column(
+    Card(
         modifier = modifier
-            .padding(horizontal = 16.dp)
-            .clip(
-                shape = RoundedCornerShape(16.dp)
-            )
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(max = 200.dp),
-            model = article.imageUrl,
-            contentDescription = stringResource(R.string.article_image),
-            contentScale = ContentScale.FillWidth
-        )
+        article.imageUrl?.let {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = 200.dp),
+                model = article.imageUrl,
+                contentDescription = stringResource(R.string.article_image),
+                contentScale = ContentScale.FillWidth
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
         Text(
-            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
+            modifier = Modifier.padding(horizontal = 16.dp),
             text = article.title,
             color = MaterialTheme.colorScheme.onSurface,
             fontWeight = FontWeight.Bold,
@@ -402,30 +411,34 @@ fun Article(
             maxLines = 2,
             overflow = TextOverflow.Ellipsis
         )
-        Text(
-            modifier = Modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp),
-            text = article.description,
-            color = MaterialTheme.colorScheme.secondary,
-            fontSize = 14.sp,
-            maxLines = 3,
-            overflow = TextOverflow.Ellipsis
-        )
+        Spacer(modifier = Modifier.height(8.dp))
+        if (article.description.isNotEmpty()) {
+            Text(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                text = article.description,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 14.sp,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
-                .padding(16.dp)
+                .padding(horizontal = 16.dp)
                 .fillMaxWidth(),
         ) {
             Text(
                 text = article.source,
-                color = MaterialTheme.colorScheme.secondary,
+                color = MaterialTheme.colorScheme.secondary.copy(alpha = 0.5f),
                 fontSize = 12.sp,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
             Text(
                 text = DateFormatter.formatDateToString(article.publishedAt),
-                color = MaterialTheme.colorScheme.secondary,
+                color = MaterialTheme.colorScheme.secondary.copy(alpha = 0.5f),
                 fontSize = 12.sp,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
@@ -447,12 +460,12 @@ fun Article(
                 }
             ) {
                 Icon(
-                    imageVector = Icons.Default.ExitToApp,
+                    imageVector = CustomIcons.OpenInNew,
                     contentDescription = stringResource(R.string.read_the_article),
                 )
                 Text(
                     modifier = Modifier.padding(horizontal = 8.dp),
-                    text = "Read"
+                    text = stringResource(R.string.read)
                 )
             }
             Button(
@@ -475,4 +488,35 @@ fun Article(
             }
         }
     }
+}
+
+@Composable
+fun SubscriptionChip(
+    modifier: Modifier = Modifier,
+    topic: String,
+    isSelected: Boolean,
+    onChipClick: (String) -> Unit,
+    onDeleteChipClick: (String) -> Unit
+) {
+    FilterChip(
+        modifier = modifier,
+        selected = isSelected,
+        onClick = {
+            onChipClick(topic)
+        },
+        label = {
+            Text(text = topic)
+        },
+        trailingIcon = {
+            Icon(
+                modifier = Modifier
+                    .size(FilterChipDefaults.IconSize)
+                    .clickable {
+                        onDeleteChipClick(topic)
+                    },
+                imageVector = Icons.Default.Clear,
+                contentDescription = stringResource(R.string.clear_chip)
+            )
+        }
+    )
 }
--- domain/src/main/java/com/ybelik/domain/usecase/AddSubscriptionUseCase.kt
+++ domain/src/main/java/com/ybelik/domain/usecase/AddSubscriptionUseCase.kt
@@ -1,12 +1,18 @@
 package com.ybelik.domain.usecase
 
 import com.ybelik.domain.repoository.NewsRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 class AddSubscriptionUseCase(
     private val repository: NewsRepository
 ) {
     suspend operator fun invoke(topic: String) {
         repository.addSubscription(topic)
-        repository.updateArticlesForTopic(topic)
+        // Запускаем обновление в отдельной корутине (=параллельно) с тем жеконтектом, то есть во viewModel scope
+        CoroutineScope(coroutineContext).launch {
+            repository.updateArticlesForTopic(topic)
+        }
     }
 }
